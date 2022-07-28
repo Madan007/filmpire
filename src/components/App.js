fc90998045732ec4c5278dc3135a81269699f7d5
Apple <apple@Apples-iMac.local>
2022-07-28 19:29:08 +0530

    Defining Project Structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,41 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => <div>App</div>;
+import useStyles from './styles';
+
+// import { Actors, Movies, Movieinformation, NavBar, Profile } from '.';
+import { Actors, Movies, Movieinformation, Profile } from '.';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      {/* <NavBar /> */}
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/">
+            <h1> Home </h1>
+          </Route>
+          <Route exact path="/movies">
+            <Movies />
+          </Route>
+          <Route exact path="/movie/:id">
+            <Movieinformation />
+          </Route>
+          <Route exact path="/actors/:movieId">
+            <Actors />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/MovieInformation/MovieInformation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Movieinformation = () => {
+  console.log('Movie Informaiton .....');
+  return (
+    <div>
+      Movie Information
+    </div>
+  );
+};
+
+export default Movieinformation;
